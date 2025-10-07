f499d871b3c29cb097a6c93bda4f27e658b69012
JanielleOliveira <janielleoliver.brito@gmail.com>
2025-10-07 14:32:32 -0300

    feat(categoria): adicionado Crud completo
    * Criado entidade Categoria
    * Injetado Repository de Categoria no CategoriaService via @InjectRepository
    * Criado service Categoria
    * Criado controller Categoria
    * Testado endpoints no Insomnia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
+import { Categoria } from './categoria/entities/categoria.entity';
 
 @Module({
   imports: [
@@ -10,10 +12,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'db_farma_code',
-      entities: [],
+      entities: [Categoria, CategoriaModule],
       synchronize: true,
       logging: true,
     }),
+    CategoriaModule,
   ],
   controllers: [],
   providers: [],
